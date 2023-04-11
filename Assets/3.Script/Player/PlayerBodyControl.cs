3d04c09440061df2adf1757761e0e52dc9814ff4
VillageManA <129142228+VillageManA@users.noreply.github.com>
2023-04-11 17:58:24 +0900

    20230411 annotation

--- Assets/3.Script/Player/PlayerBodyControl.cs
+++ Assets/3.Script/Player/PlayerBodyControl.cs
@@ -9,6 +9,8 @@ public class PlayerBodyControl : MonoBehaviour
 
     void Update()
     {
+        // W,A,S,D 를이용해 방향으로 움직이는 부분
+        // 머리방향과 몸방향,몸의 움직임을 담당함
         float MoveX = Input.GetAxisRaw("MoveX");
         float MoveY = Input.GetAxisRaw("MoveY");
 
@@ -25,6 +27,8 @@ public class PlayerBodyControl : MonoBehaviour
             animator.SetFloat("MoveY", MoveY);
         }
 
+        // 에셋이 위쪽과 왼쪽밖에 없어서 로테이트값을 줘서 아래와 오른쪽을 갈땐 에셋을 돌려서 사용
+        // 키다운을 떼면 다시 로테이트값을 360도로 원상복귀시켜서 위쪽과 왼쪽을갈때 정상적으로 작동하게 하였음.
         if (Input.GetKeyDown(KeyCode.S))
         {
             gameObject.transform.Rotate(0, 180, 0);
--- Assets/3.Script/Player/PlayerHeadControl.cs
+++ Assets/3.Script/Player/PlayerHeadControl.cs
@@ -8,6 +8,7 @@ public class PlayerHeadControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // W,A,S,D 를 이용하여 머리의 움직임만 이용할수있는부분
         float MoveX = Input.GetAxisRaw("MoveX");
         float MoveY = Input.GetAxisRaw("MoveY");
 
@@ -24,7 +25,8 @@ public class PlayerHeadControl : MonoBehaviour
             animator.SetFloat("MoveY", MoveY);
         }
 
-
+        // 방향키를 사용하여 머리에서 공격이 나갈타이밍에 애니메이션을 나가도록하는부분
+        // 추후 딜레이랑 이런건 코루틴을 사용해서 변경예정
         float AttackX = Input.GetAxisRaw("Horizontal");
         float AttackY = Input.GetAxisRaw("Vertical");
 
